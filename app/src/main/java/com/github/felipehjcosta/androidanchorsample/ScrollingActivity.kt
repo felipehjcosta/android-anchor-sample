1b369ca965af30d11f32ad5645278193ba55151e
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-01-07 21:40:49 -0200

    Refactoring anchor strategy

--- app/src/main/java/com/github/felipehjcosta/androidanchorsample/ScrollingActivity.kt
+++ app/src/main/java/com/github/felipehjcosta/androidanchorsample/ScrollingActivity.kt
@@ -1,79 +1,78 @@
 package com.github.felipehjcosta.androidanchorsample
 
+import android.app.Activity
 import android.os.Bundle
+import android.support.annotation.IdRes
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.util.DisplayMetrics
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import kotlinx.android.synthetic.main.scrolling_activity.*
 
 
-class ScrollingActivity : AppCompatActivity() {
+class ScrollingActivity : AppCompatActivity(), ObservableScrollView.ObservableScrollViewCallbacks {
 
-    private val callback = object : ObservableScrollView.ObservableScrollViewCallbacks {
-        override fun onScrollChanged(deltaY: Int) {
-            val scrollView = findViewById<ObservableScrollView>(R.id.observable_scroll_view)
-            val scrollY = scrollView.scrollY
-            val toolbar = findViewById<View>(R.id.toolbar)
+    private val toolbar by bindView<Toolbar>(R.id.toolbar)
 
-            val placeHolder = findViewById<View>(R.id.place_holder)
+    private val observableScrollView by bindView<ObservableScrollView>(R.id.observable_scroll_view)
 
-            val location = intArrayOf(0, 0)
-            placeHolder.getLocationOnScreen(location)
-
-
-            val dm = DisplayMetrics().apply { windowManager.defaultDisplay.getMetrics(this) }
-            val topContainer = findViewById<View>(R.id.top_container)
-            val topOffset = (dm.heightPixels - topContainer.measuredHeight) + toolbar.height
-            android.util.Log.e("SCROLLING", ">>>>> deltaY: ${deltaY} " +
-                    "\nscrollY: ${scrollY} " +
-                    "\nheight: ${scrollView.height} " +
-                    "\nlocation[1]: ${location[1]}" +
-                    "\ntopOffset: ${topOffset}" +
-                    "\nplace holder top:  ${placeHolder.top} " +
-                    "\nplace holder height:  ${placeHolder.height} " +
-                    "\nplace holder bottom:  ${placeHolder.bottom} "
-            )
-
-            if (location[1] > dm.heightPixels) {
-                // OUT TOP
-                android.util.Log.e("SCROLLING", ">>>>> OUT TOP")
-                anchor.translationY = ((anchor.parent as ViewGroup).height - anchor.height).toFloat()
-            } else if (location[1] < 0) {
-                // OUT BOTTOM
-                android.util.Log.e("SCROLLING", ">>>>> OUT BOTTOM")
-                anchor.translationY = (location[1] - topOffset).toFloat()
-            } else {
-                // SHOW
-
-                val topLocation = dm.heightPixels
-                val bottomLocation = dm.heightPixels - anchor.height
-                var delta = 0
-                if (location[1] > bottomLocation) {
-                    delta = location[1] - bottomLocation
-                }
-                android.util.Log.e("SCROLLING", ">>>>> SHOW")
-                anchor.translationY = (location[1] - topOffset - delta).toFloat()
-            }
-        }
-    }
+    private val placeHolder by bindView<View>(R.id.place_holder)
+    private val topContainer by bindView<View>(R.id.top_container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.scrolling_activity)
         setSupportActionBar(toolbar)
 
-        findViewById<ObservableScrollView>(R.id.observable_scroll_view)?.run {
-            addScrollViewCallbacks(callback)
+        observableScrollView.run {
+            addScrollViewCallbacks(this@ScrollingActivity)
+
+            viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    onScrollChanged(0)
+                }
+            })
         }
     }
 
+    override fun onScrollChanged(deltaY: Int) {
+
+        val placeHolderYPositionOnScreen = intArrayOf(0, 0).apply { placeHolder.getLocationOnScreen(this) }[1]
+
+        val screenHeight = screenHeight()
+        val topOffset = calculateScrollViewTop(screenHeight)
+
+        anchor.translationY = if (isScreenBelow(placeHolderYPositionOnScreen, screenHeight)) {
+            ((anchor.parent as ViewGroup).height - anchor.height).toFloat()
+        } else if (isScreenAbove(placeHolderYPositionOnScreen)) {
+            (placeHolderYPositionOnScreen - topOffset).toFloat()
+        } else {
+            val bottomLocation = screenHeight - anchor.height
+            val delta = if (placeHolderYPositionOnScreen > bottomLocation) {
+                placeHolderYPositionOnScreen - bottomLocation
+            } else {
+                0
+            }
+            (placeHolderYPositionOnScreen - topOffset - delta).toFloat()
+        }
+    }
+
+    private fun screenHeight(): Int = with(DisplayMetrics()) {
+        windowManager.defaultDisplay.getMetrics(this)
+        heightPixels
+    }
+
+    private fun calculateScrollViewTop(screenHeight: Int) = (screenHeight - topContainer.measuredHeight) + toolbar.height
+
+    private fun isScreenBelow(placeHolderYPositionOnScreen: Int, screenHeight: Int) = placeHolderYPositionOnScreen > screenHeight
+
+    private fun isScreenAbove(placeHolderYPositionOnScreen: Int) = placeHolderYPositionOnScreen < 0
+
     override fun onDestroy() {
         super.onDestroy()
-        findViewById<ObservableScrollView>(R.id.observable_scroll_view)?.run {
-            removeScrollViewCallbacks(callback)
+        observableScrollView.run {
+            removeScrollViewCallbacks(this@ScrollingActivity)
         }
     }
 
@@ -86,5 +85,9 @@ class ScrollingActivity : AppCompatActivity() {
         R.id.action_settings ->
             true
         else -> super.onOptionsItemSelected(item)
+    }
+
+    inline fun <reified T : View> Activity.bindView(@IdRes idRes: Int): Lazy<T> {
+        return lazy(LazyThreadSafetyMode.NONE) { findViewById<T>(idRes) as T }
     }
 }
