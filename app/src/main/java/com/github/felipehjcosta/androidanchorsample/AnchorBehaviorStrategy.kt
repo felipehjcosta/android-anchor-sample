70202f4b8fb1e6f6bf064dbf745b4a0d8ff4ee35
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-01-08 22:04:31 -0200

    Refactoring anchor strategy

--- app/src/main/java/com/github/felipehjcosta/androidanchorsample/AnchorBehaviorStrategy.kt
+++ app/src/main/java/com/github/felipehjcosta/androidanchorsample/AnchorBehaviorStrategy.kt
@@ -9,46 +9,59 @@ class AnchorBehaviorStrategy(
 ) : ObservableScrollView.ObservableScrollViewCallbacks {
 
     override fun onScrollChanged(observableScrollView: ObservableScrollView, deltaY: Int) {
-        val topOffset = calculateScrollViewTop(observableScrollView)
+        val scrollViewTopOnScreen = calculateScrollViewTopOnScreen(observableScrollView)
+        setupPivot()
+        translateVertically(scrollViewTopOnScreen)
+        scaleBasedOnHeight()
+    }
 
+    private fun setupPivot() {
         anchor.pivotX = anchor.width / 2.0f
         anchor.pivotY = anchor.height / 2.0f
-        anchor.translationY = calculateVerticalTranslation(topOffset).toFloat()
-        anchor.scaleX = calculateScale()
-        anchor.scaleY = calculateScale()
     }
 
-    private fun calculateVerticalTranslation(topOffset: Int): Int = when {
-        isPlaceHolderBelowScrollViewport() -> (anchor.parent as ViewGroup).height - anchor.height
-        isPlaceHolderAboveScrollViewport() -> placeHolderYPositionOnScreen - topOffset
-        else -> {
-            val bottomLocation = screenHeight - anchor.height
-            val delta = if (placeHolderYPositionOnScreen > bottomLocation) {
-                placeHolderYPositionOnScreen - bottomLocation
-            } else {
-                0
+    private fun translateVertically(scrollViewTopOnScreen: Int) {
+        val translation = when {
+            isPlaceHolderBelowScrollViewport() -> (anchor.parent as ViewGroup).height - anchor.height
+            isPlaceHolderAboveScrollViewport() -> placeHolderTopOnScreen - scrollViewTopOnScreen
+            else -> {
+                val anchorTop = screenHeight - anchor.height
+                val delta = if (placeHolderTopOnScreen > anchorTop) {
+                    placeHolderTopOnScreen - anchorTop
+                } else {
+                    0
+                }
+                placeHolderTopOnScreen - scrollViewTopOnScreen - delta
             }
-            placeHolderYPositionOnScreen - topOffset - delta
+        }
+
+        anchor.translationY = translation.toFloat()
+    }
+
+    private fun scaleBasedOnHeight() {
+        val scale = when {
+            isPlaceHolderBelowScrollViewport() -> 1.0f
+            isPlaceHolderAboveScrollViewport() -> 1.0f
+            else -> {
+                val anchorTop = screenHeight - anchor.height
+                val delta = if (placeHolderTopOnScreen > anchorTop) {
+                    placeHolderTopOnScreen - anchorTop
+                } else {
+                    0
+                }
+
+                val scale = delta.toFloat() / anchor.height
+                if (scale < 0.95f) 0.95f else scale
+            }
+        }
+
+        anchor.apply {
+            scaleX = scale
+            scaleY = scale
         }
     }
 
-    private fun calculateScale(): Float = when {
-        isPlaceHolderBelowScrollViewport() -> 1.0f
-        isPlaceHolderAboveScrollViewport() -> 1.0f
-        else -> {
-            val bottomLocation = screenHeight - anchor.height
-            val delta = if (placeHolderYPositionOnScreen > bottomLocation) {
-                placeHolderYPositionOnScreen - bottomLocation
-            } else {
-                0
-            }
-
-            val scale = delta.toFloat() / anchor.height
-            if (scale < 0.95f) 0.95f else scale
-        }
-    }
-
-    private val placeHolderYPositionOnScreen
+    private val placeHolderTopOnScreen
         get() = with(intArrayOf(0, 0)) {
             placeHolder.getLocationOnScreen(this)
             this[1]
@@ -57,12 +70,12 @@ class AnchorBehaviorStrategy(
     private val screenHeight: Int
         get() = anchor.resources.displayMetrics.heightPixels
 
-    private fun calculateScrollViewTop(scrollView: ObservableScrollView): Int = with(intArrayOf(0, 0)) {
+    private fun calculateScrollViewTopOnScreen(scrollView: ObservableScrollView): Int = with(intArrayOf(0, 0)) {
         scrollView.getLocationOnScreen(this)
         this[1]
     }
 
-    private fun isPlaceHolderBelowScrollViewport() = placeHolderYPositionOnScreen > screenHeight
+    private fun isPlaceHolderBelowScrollViewport() = placeHolderTopOnScreen > screenHeight
 
-    private fun isPlaceHolderAboveScrollViewport() = placeHolderYPositionOnScreen < 0
+    private fun isPlaceHolderAboveScrollViewport() = placeHolderTopOnScreen < 0
 }
